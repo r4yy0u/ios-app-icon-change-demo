f5cb437134159ee295b2f83e3290733e77afefa7
Rui You <medopaw@gmail.com>
2023-05-26 19:52:29 +0800

    Initial Commit

---
+++ ios-app-icon-change-demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios-app-icon-change-demo
+//
+//  Created by yourui on 2023/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-app-icon-change-demo/ios_app_icon_change_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_app_icon_change_demoApp.swift
+//  ios-app-icon-change-demo
+//
+//  Created by yourui on 2023/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_app_icon_change_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
